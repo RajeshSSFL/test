d71ebc94da464e846b9b99742d82816d179b06be
Prasenjit <prasenjit.aluni@gmail.com>
2025-01-18 06:38:34 +0530

    Test

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ console.log('\n\n-: App Started :-');
 const express   = require('express');
 const app       = express();
 
+app.use('/test', function(){
+    console.log(first);
+});
+
+
 app.use('/', (req, res, next)=>{
     console.log('-: Welcome :-');
     res.send('-: Welcome :-');
